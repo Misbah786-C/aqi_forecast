bb7e74e777b61c4ad51f3b18da5ae0cb15bb85df
Misbah Azhar <123809958+Misbah786-C@users.noreply.github.com>
2025-10-27 15:53:14 +0500

    updated

--- trainings/train_sklearn.py
+++ trainings/train_sklearn.py
@@ -22,11 +22,12 @@ logger = logging.getLogger(__name__)
 # ------------------------------------------------
 # Load environment variables
 # ------------------------------------------------
-load_dotenv()
+HOPSWORKS_API_KEY = os.environ.get("AQI_FORECAST_API_KEY")  # must match your secret name
 
-HOPSWORKS_API_KEY = os.getenv("aqi_forecast_api_key")
 if not HOPSWORKS_API_KEY:
-    raise ValueError("❌ Missing Hopsworks API key! Please set 'aqi_forecast_api_key' in your .env file.")
+    raise ValueError(
+        "❌ Missing Hopsworks API key! Please set 'AQI_FORECAST_API_KEY' as a GitHub Secret."
+    )
 
 # ------------------------------------------------
 # Connect to Hopsworks
